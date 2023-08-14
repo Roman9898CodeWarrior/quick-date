8d4d2db2ffca969dcbe7ede41527f5124159ae70
YourName <name@mail.ru>
2023-08-14 17:51:39 +0300

    feat: add basic functionality (Monday, August 14, 2023 17:52)

---
+++ src/components/qualities.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Qualities = () => {};
+
+export default Qualities;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
+import reportWebVitals from "./reportWebVitals";
+import Users from "./components/users";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Users />
+  </React.StrictMode>
+);
+
+reportWebVitals();
